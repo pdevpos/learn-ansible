ac1eff4ebc60f11be42bf5b141e92dae34d2614f
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-11-22 18:08:14 +0530

    host address changed

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# learn-ansible
+# expense-ansible
---
+++ ansible.sh
@@ -0,0 +1,13 @@
+component=$1
+env=$2
+# shellcheck disable=SC2154
+#ansible-playbook  get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component
+ansible-playbook  get_secrets_vault.yml -e vault_token="hvs.fMGVYmkvteLqprw3itd1iXUe" -e component_name=$component
+ansible-playbook -i 172.31.35.208, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
+ansible-playbook -i 172.31.35.208, expense.yml -e component_name=$component -e env=$env -e '@secrets.json'
+
+#aws ec2 describe-instances --filters "Name=tag:Name,Values=$component-$env"  --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >inv
+#ansible-playbook -i inv, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
+#ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json's
+
+
