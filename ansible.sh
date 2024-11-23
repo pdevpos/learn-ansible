f6aec8a18e889972375261e5c0451969eb9180a9
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-11-23 19:10:20 +0530

    loop ansible vault

--- ansible.sh
+++ ansible.sh
@@ -3,7 +3,7 @@ env=$2
 vault_token=$3
 # shellcheck disable=SC2154
 ansible-playbook  get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component
-ansible-playbook -i 172.31.35.208, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json' -e '@~/app.json'
+#ansible-playbook -i 172.31.35.208, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json' -e '@~/app.json'
 ansible-playbook -i 172.31.35.208, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json'
 
 #aws ec2 describe-instances --filters "Name=tag:Name,Values=$component-$env"  --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >inv
