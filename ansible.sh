567dae92ed498d135398e0ac4fea2b60a1cc22cf
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-11-26 14:45:32 +0530

    removed ;

--- ansible.sh
+++ ansible.sh
@@ -6,6 +6,7 @@ rm -f ~/*.json
 ansible-playbook   get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component
 #ansible-playbook -i 172.31.44.89, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json' -e '@~/app.json'
 ansible-playbook -i 172.31.44.89, expense.yml -e component_name=$component -e env=$env  -e '@~/secrets.json'
+rm -f ~/*.json
 
 #aws ec2 describe-instances --filters "Name=tag:Name,Values=$component-$env"  --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >inv
 #ansible-playbook -i inv, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
