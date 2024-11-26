2177075be41cc1612cfad94ca282745bd2d4fb64
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-11-26 14:15:47 +0530

    removed ;

--- ansible.sh
+++ ansible.sh
@@ -2,6 +2,7 @@ component=$1
 env=$2
 vault_token=$3
 # shellcheck disable=SC2154
+rm -f ~/*.json
 ansible-playbook  get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component
 #ansible-playbook -i 172.31.44.89, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json' -e '@~/app.json'
 ansible-playbook -i 172.31.44.89, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json'
